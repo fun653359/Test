4c51847cbf8b76f3c29ed8d668e4602bc1662998
fun653359 <fun653359@gmail.com>
2026-04-08 19:16:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
-print("김태건")
-print("김태건")
+# height = float(input("안녕하세요 키를 입력하세요 : "))
+# print(height + 5)
+
+# for i in range(1,6):
+#     print(i)
+
+# for x in range(10,0,-1):
+#     print(x)
+# i = 0
+# while i < 100:
+#     print("Hello, world!")
+#     i += 1
+
+# i = int(input("숫자를 입력하세요"))
+# if i % 3 == 0:
+#     print("잘했어요")
+# elif:
+#     print("잘할뻔 했어요")
+# else:
+#     print("못했어요")
+
+count = 10
+
+def count_number():
+    global count
+    count = 20
+    print(count)
+
+count_number()
+print(count)

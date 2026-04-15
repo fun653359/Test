81fae8bb5b809037816a8a65cb19939f2cb7c9f0
fun653359 <fun653359@gmail.com>
2026-04-15 15:32:01 +0900

    아나 과제

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
-# height = float(input("안녕하세요 키를 입력하세요 : "))
-# print(height + 5)
+from datetime import date
+class 학생:
+    def __init__(self,name,age,school_number,birth):
+        self.name = name
+        self.age=age
+        self.school_number=school_number
+        self.birth = birth
+    def introduce(self):
+        print(f"안녕하세요 {self.name}입니다 나이는 {self.age}이고 학번은 {self.school_number} 이고 생일은 {self.birth}입니다")
+    def school(self):
+        return date.today().weekday() < 5
 
-# for i in range(1,6):
-#     print(i)
 
-# for x in range(10,0,-1):
-#     print(x)
-# i = 0
-# while i < 100:
-#     print("Hello, world!")
-#     i += 1
+class birth(학생):
+    def __init__(self,name,age,school_number,birth):
+        self.name = name
+        self.age=age
+        self.school_number=school_number
+        self.birth = birth
+    def find_birth(self,day):
+        if day == self.birth:
+            print("생신축하드려요")
+        else:
+            print("생신이 아니네요...아쉬워요")
+a = 학생("홍정현",17,10621,"5050")
+'''if __name__ == "__main__":'''
 
-# i = int(input("숫자를 입력하세요"))
-# if i % 3 == 0:
-#     print("잘했어요")
-# elif:
-#     print("잘할뻔 했어요")
-# else:
-#     print("못했어요")
-
-def double_number():
-    count = int(input("제곱할 수를 입력하세요"))
-    num = int(input("몇번 제곱할건지 정하세요"))
-    print(count ** num)
-
-double_number()
+b = birth("홍정현",17,10621,"5050")
+a.introduce()
+b.find_birth("5050")
